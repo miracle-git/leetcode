64a9c0ae86e34b3005d71e90ae92904bea8c19e1
Miracle He <hmiinyu@sina.com>
2022-05-15 20:42:22 +0800

    更新测试配置

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reporters: [
     "default",
     ["./node_modules/jest-html-reporter", {
-      "pageTitle": "Data Algorithm Test Report"
+      "pageTitle": "Leetcode Algorithm Test Report"
     }]
   ]
 }
