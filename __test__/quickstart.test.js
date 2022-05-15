6075ab679f251d32f42167c5db2187bee36153ce
Miracle He <hmiinyu@sina.com>
2022-05-15 21:42:54 +0800

    更新文档

--- __test__/quickstart.test.js
+++ __test__/quickstart.test.js
@@ -6,7 +6,7 @@ test('测试：查找唯一数字', () => {
   expect(singleNumber([3, 4, 1, 2, 3, 3, 4, 1, 2, 3, 4])).toEqual(4)
 })
 
-test('测试：查找大多数数字', () => {
+test('测试：查找众数', () => {
   expect(majorityNumber([2, 2, 1])).toEqual(2)
   expect(majorityNumber([4, 1, 2, 1, 2, 2])).toEqual(2)
   expect(majorityNumber([3, 4, 1, 2, 3, 3, 4, 1, 2, 3, 4])).toEqual(3)
