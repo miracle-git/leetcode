857c8e1f502849fcb1470e3cc100ad07ee567e88
Miracle He <miracle@MiracledeMacBook-Pro.local>
2023-03-11 03:22:32 +0800

    更新文档

--- array/index.js
+++ array/index.js
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/two-sum/
+// https://leetcode.com/problems/two-sum
 export const twoSum = {
   v1: require('./two-sum/v1').default,
   v2: require('./two-sum/v2').default,
