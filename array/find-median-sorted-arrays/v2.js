c12dff74b903e6d80c759fae2b2dca98ad8fb845
Miracle He <miracle@MiracledeMacBook-Pro.local>
2023-03-12 22:52:08 +0800

    更新寻找排序数组的中位数

--- array/find-median-sorted-arrays/v2.js
+++ array/find-median-sorted-arrays/v2.js
@@ -7,10 +7,11 @@ export default function findMedianSortedArrays(nums1, nums2) {
   if (n === 0) return findMedianSortedArrays(nums2, nums1)
   if (m + n <= 2 && m && n) return (nums1[0] + nums2[0]) / 2
 
-  const max1 = nums1[m - 1]
-  const max2 = nums2[n - 1]
-  nums1[0] < nums2[0] ? nums1.shift() : nums2.shift()
-  max1 > max2 ? nums1.pop() : nums2.pop()
+  // 注意在"掐头去尾"前保存对应的状态(否则容易掐错位)
+  const isShift = nums1[0] < nums2[0]
+  const isPop = nums1[m - 1] > nums2[n - 1]
+  isShift ? nums1.shift() : nums2.shift()
+  isPop ? nums1.pop() : nums2.pop()
 
   return findMedianSortedArrays(nums1, nums2)
 }
