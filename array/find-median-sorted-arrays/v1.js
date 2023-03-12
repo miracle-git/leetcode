d27a09fdf9af529bad009a3fda08ce9d11984c62
Miracle He <miracle@MiracledeMacBook-Pro.local>
2023-03-12 23:00:15 +0800

    更新寻找排序数组的中位数

--- array/find-median-sorted-arrays/v1.js
+++ array/find-median-sorted-arrays/v1.js
@@ -1,6 +1,5 @@
 export default function findMedianSortedArray(nums1, nums2) {
   const merge = [...nums1, ...nums2].sort((a, b) => a - b)
   const len = merge.length, pos = Math.floor(len / 2)
-  if (len % 2) return merge[pos]
-  return (merge[pos - 1] + merge[pos]) / 2
+  return len % 2 ? merge[pos] : (merge[pos - 1] + merge[pos]) / 2
 }
